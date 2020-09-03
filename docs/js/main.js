26dd1d771d4712cf49b0aafe3b9cc214f5f626ec
Valeriy <leshikzoloto@gmail.com>
2020-09-03 15:39:12 +0300

    js add

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,50 +1,53 @@
- // fake-placeholder
+$(document).ready(function () {
 
- const formInputs = document.querySelectorAll(".form-field");
+    //-фильтрация проектов
+    let containerEl = document.querySelector('#portfolio-projects');
 
- for (let item of formInputs) {
-     const inputPlaceholder = item.nextElementSibling;
+    let mixer = mixitup(containerEl, {
+        classNames: {
+            block: ""
+        }
+    });
 
-     item.addEventListener("focus", function () {
-         inputPlaceholder.classList.add("active");
-     });
+    // - выравниваем   ширину карточек при фильтрации
+    const filterToggles = document.querySelectorAll('.portfolio-works-toggle button'); // все кнопки-переключатели
+    const portfolioBigCards = document.querySelectorAll('.portfolio-works__item '); // вс карточки  работ
 
-     item.addEventListener("blur", function(){
-         if(this.value == ""){
-             inputPlaceholder.classList.remove("active");
-         }
-     })
- };
+    for (let i = 0; i < filterToggles.length; i++) {
 
-      let containerE1 = document.querySelector("#portfolio-projects");
+        filterToggles[i].addEventListener('click', function () {
+            if (i == 0) {
 
-      let mixer = mixitup(containerE1, {
-          classNames: {
-              block: ""
-          }
-      });
+                for (let j = 0; j < 2; j++) {
 
-    //  // фильтрация проектов
-      let containerE1 = document.querySelector("#portfolio-projects");
+                    portfolioBigCards[j].classList.add('portfolio-works__item--big')
+                }
 
-      let mixer = mixitup(containerE1, {
-          classNames: {
-              block: ""
-          }
-      });
+            } else {
 
-      let filterToggles = document.querySelectorAll(".portfolio-works-toggle button");
-      let portfolioBigCards = document.querySelectorAll(".portfolio-card");
- 
-      for (let i = 0; i < filterToggles.length; i++) {
-          filterToggles[i].addEventListener("click", function (){
-              if (i ==0) {
-                  for (let j = 0; j < 2; j++){
-                      portfolioBigCards[j].classList.add("portfolio-card--big")
-                  }
-              } else {
-                  for (let j = 0; j < 2; j++){
-                      portfolioBigCards[j].classList.remove("portfolio-card--big")
-                  }
-              }
-         });
+                for (let j = 0; j < 2; j++) {
+                    portfolioBigCards[j].classList.remove('portfolio-works__item--big')
+                }
+            }
+        });
+    }
+
+
+})
+// fake-placeholder
+
+const formInputs = document.querySelectorAll(".form-field");
+
+for (let item of formInputs) {
+    const inputPlaceholder = item.nextElementSibling;
+
+    item.addEventListener("focus", function () {
+        inputPlaceholder.classList.add("active");
+    });
+
+    item.addEventListener("blur", function(){
+        if(this.value == ""){
+            inputPlaceholder.classList.remove("active");
+        }
+    })
+};
