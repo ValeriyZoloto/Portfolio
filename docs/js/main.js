087eb5b0784b8c46604f0206746b6d2cfb6afe76
Valeriy <leshikzoloto@gmail.com>
2020-09-06 12:11:19 +0300

    add page nav

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,43 @@
 $(document).ready(function () {
+     //  PAGE NAV
+
+     $("#page-nav").onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollIhreshold: 0.5,
+        filter: "",
+        easing: "swing",
+        begin: function(){},
+        end: function(){},
+        scrollChange: function ($currentListItem){}
+    });
+    
+    // 1. получить кнопку по которой будет клик
+    // 2. получить мобильное меню
+    // 3. прослушать клик по кнопке и по клику то добавлять, то удалять класс active у мобил.меню
+        // 4.то добавлять, то удалять класс active у кнопки, по которой кликаем
+    // 5. получить оверлей
+    // 6.при клике по кнопке, то добавлять, то удалять класс active
+
+    // 1.прослушать событие резайз у окна
+    // 2. у всех дивов убрать класс active
+
+    let mobileMenuToggle = document.querySelector('.toggle-menu');
+    let mobMenu = document.querySelector(".header-menu");
+    let overlay = document.querySelector("#overlay");
+
+    mobileMenuToggle.addEventListener("click",function(){
+        mobMenu.classList.toggle("active");
+        this.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+    window.addEventListener("resize",function(){
+        mobMenu.classList.remove('active');
+        mobileMenuToggle.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
 
     //-фильтрация проектов
     let containerEl = document.querySelector('#portfolio-projects');
